e19aa11d1cc30870804cafcf1aa557025ea43c08
MathieuGay <117897168+MathieuGay@users.noreply.github.com>
2022-12-08 12:04:10 +0100

    Fichiers du site - version 3

---
+++ projet-jura-2023/head.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<?php
+require 'connexion_bdd.php';
+?>
+
+
+<html lang="fr" dir="ltr">
+<meta charset="utf-8" />
+<!--Head du site-->
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <title>Projet Jura</title>
+  <!-- Bootstrap icons-->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container px-4 px-lg-5">
+      <a class="navbar-brand" href="#!">Projet Jura</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+          <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Accueil</a></li>
+          <li class="nav-item"><a class="nav-link" href="page_connexion.php">Connexion</a></li>
+          </li>
+        </ul>
+        </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+  </ul>
+  </div>
