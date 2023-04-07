c7d6f000a32baef5b5c4dbe8551d51700def5c7f
MathieuGay <117897168+MathieuGay@users.noreply.github.com>
2023-04-07 22:30:24 +0200

    Delete config.php

--- projet-jura-v6/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('PATH', str_replace($_SERVER['DOCUMENT_ROOT'], $_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_PORT'], "http://".__DIR__."/"));
-define ('DB_HOST', "localhost");
-define ('DB_USER', "root");
-define ('DB_PWD', "");
-define ('DB_NAME', "projet_jura");
-?>
